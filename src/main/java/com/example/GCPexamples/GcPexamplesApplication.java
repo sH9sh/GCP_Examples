02acf8c87aa3442d3b63818cc426a842aef64896
Seán Hester <sean.hester@zinkworks.com>
2024-05-22 11:48:22 +0100

    first commit

---
+++ src/main/java/com/example/GCPexamples/GcPexamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.GCPexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcPexamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcPexamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GCPexamples/GcPexamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GCPexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcPexamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
